27b607845c400ff34cff28d8b885d1e98506faf0
shzx1994529 <weaponxxx1994@163.com>
2019-09-15 17:47:43 +0800

    添加测试配置

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,27 @@
+  
+// https://jestjs.io/docs/en/configuration.html
+
 module.exports = {
   verbose: true,
   clearMocks: false,
-  collectCoverage: true,
-  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
   reporters: ["default"],
+
+  collectCoverage: false,
+
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  moduleDirectories: ['node_modules', 'include'],
-  testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
-  moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|mp3||m4a|aac|oga|webm|wav)$": "<rootDir>/test/__mocks__/file-mock.js",
-    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js"
+  moduleDirectories: ['node_modules'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+    },
   },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
+    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",
+  },
+  testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
   transform: {
-    '^.+unit\\.(js|jsx)$': 'babel-jest',
+    "^.+unit\\.(js|jsx)$": "babel-jest",
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   setupFilesAfterEnv: ["<rootDir>test/setupTests.js"]
